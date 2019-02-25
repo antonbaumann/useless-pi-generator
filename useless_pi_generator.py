db50c3967988ce727e1145d48f92f9106655baad
Anton Baumann <antonbaumann4@gmail.com>
2019-02-25 11:40:12 +0100

    implemented euklid

--- useless_pi_generator.py
+++ useless_pi_generator.py
@@ -1,13 +1,18 @@
-import math
 import random
+import math
 import sys
+
+def gcd(a, b):
+	while b > 0:
+		a, b = b, a % b
+	return a
 
 def generate_pi(iterations=100000):
 	count_coprime = 0
 	for _ in range(iterations):
 		rand_1 = random.randint(1, sys.maxsize)
 		rand_2 = random.randint(1, sys.maxsize)
-		if math.gcd(rand_1, rand_2) == 1:
+		if gcd(rand_1, rand_2) == 1:
 			count_coprime += 1
 	return math.sqrt(6 / (count_coprime / iterations))
 
